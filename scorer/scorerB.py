1bdbc3b95a7c63a4676dd2fa889831a79da0edcb
ncwphilly <wnoah@sas.upenn.edu>
2018-03-28 05:12:26 -0400

    completed eval script

--- scorer/scorerB.py
+++ scorer/scorerB.py
@@ -13,38 +13,59 @@ observed = 0
 correct = 0
 total = len(truth_values.keys())
 errors = []
+tp = 0
+fp = 0
+tn = 0
+fn = 0
 
 print(len(truth_values), 'entries in reference file')
 
 for reference_id in truth_values.keys():
-	if reference_id in submission.keys():
-		print('matching entry:', reference_id)
-		observed += 1
-		try:
-			yhat, confidence = submission[reference_id]
-		except ValueError:
-			print('   Each entry should be a list of two values - [veracity, confidence]')
-			print('   veracity is one of "true" or "false"; confidence is a float from 0..1.')
-			print('   This entry was:', submission[reference_id], ',  for document key', reference_id)
-			sys.exit('-- error: data format: stopping')
+    if reference_id in submission.keys():
+        print('matching entry:', reference_id)
+        observed += 1
+        try:
+            yhat, confidence = submission[reference_id]
+        except ValueError:
+            print('   Each entry should be a list of two values - [veracity, confidence]')
+            print('   veracity is one of "true" or "false"; confidence is a float from 0..1.')
+            print('   This entry was:', submission[reference_id], ',  for document key', reference_id)
+            sys.exit('-- error: data format: stopping')
 
-		if yhat == truth_values[reference_id]:
-			correct += 1
-			errors.append( (1-confidence) ** 2 )
+        if yhat == truth_values[reference_id]:
+            correct += 1
+            errors.append((1 - confidence) ** 2)
+            if yhat:
+                tp += 1
+            else:
+                tn += 1
 
-		elif truth_values[reference_id] == 'unverified':
-			errors.append( (confidence) ** 2 )
-		
-		else:
-			errors.append(1.0)
-	else:
-		print('unmatched entry:', reference_id, '-- no reference value for this document')
+        elif truth_values[reference_id] == 'unverified':
+            errors.append((confidence) ** 2)
+
+        else:
+            errors.append(1.0)
+            if yhat:
+                fp += 1
+            else:
+                fn += 1
+    else:
+        print('unmatched entry:', reference_id, '-- no reference value for this document')
 
 score = correct / total
-rmse = math.sqrt( sum(errors) / len(errors) )
+rmse = math.sqrt(sum(errors) / len(errors))
+
+
+precision = tp / float(tp + fp)
+recall = tp / float(tp + fn)
+fscore = (2 * precision * recall) / float(precision + recall)
 
 print(observed, 'matched entries in submission')
 print(total, 'entries in reference file')
 
 print('veracity accuracy:', score)
 print('confidence rmse:  ', rmse)
+
+print('precision:', precision)
+print('recall:', recall)
+print('F1:', fscore)
