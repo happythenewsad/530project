7ec502cc01dee3433801281ed5fca0b50c347126
Peter Kong <peter@peterkong.com>
2018-04-07 16:55:43 -0400

    added test harness

---
+++ test_runner.py
@@ -0,0 +1,9 @@
+import os
+import unittest
+import logging
+import sys
+
+#sys.path.insert(1, "./lib")
+
+all_tests = unittest.TestLoader().discover(os.path.join(os.path.dirname(__file__), "tests"), "test_*.py")
+unittest.TextTestRunner(verbosity=2).run(all_tests)
