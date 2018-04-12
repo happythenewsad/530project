3b6098939df35c36e20bf600b5826cefa19ad80a
ncwphilly <wnoah@sas.upenn.edu>
2018-04-11 21:56:58 -0400

    Deleted erroneous variables, added f1_micro
    F1_micro seems to be the same as score which is just correct/total

--- scorer/score.py
+++ scorer/score.py
@@ -3,6 +3,7 @@ import json
 import math
 import os.path
 import sys
+from sklearn.metrics import f1_score
 
 [_, reference_file, submission_file] = sys.argv
 
@@ -13,11 +14,13 @@ observed = 0
 correct = 0
 total = len(truth_values.keys())
 errors = []
-tp = 0
-fp = 0
-tn = 0
-fn = 0
+#tp = 0
+#fp = 0
+#tn = 0
+#fn = 0
 
+yhats = []
+truths = []
 print(len(truth_values), 'entries in reference file')
 
 for reference_id in truth_values.keys():
@@ -25,7 +28,11 @@ for reference_id in truth_values.keys():
         print('matching entry:', reference_id)
         observed += 1
         try:
+     
             yhat, confidence = submission[reference_id]
+            yhats.append(yhat)
+            truths.append(truth_values[reference_id])
+
         except ValueError:
             print('   Each entry should be a list of two values - [veracity, confidence]')
             print('   veracity is one of "true" or "false"; confidence is a float from 0..1.')
@@ -35,20 +42,29 @@ for reference_id in truth_values.keys():
         if yhat == truth_values[reference_id]:
             correct += 1
             errors.append((1 - confidence) ** 2)
-            if yhat:
+            """
+            if yhat == 'true':
                 tp += 1
-            else:
+            elif yhat == 'false':
                 tn += 1
-
+            """
         elif truth_values[reference_id] == 'unverified':
             errors.append((confidence) ** 2)
+            """
+            if yhat == 'true':
+                fp += 1
+            elif yhat == 'false':
+                fn += 1
+            """
 
         else:
             errors.append(1.0)
-            if yhat:
+            """
+            if yhat == 'true':
                 fp += 1
-            else:
+            elif yhat == 'false':
                 fn += 1
+            """
     else:
         print('unmatched entry:', reference_id, '-- no reference value for this document')
 
@@ -56,16 +72,13 @@ score = correct / total
 rmse = math.sqrt(sum(errors) / len(errors))
 
 
-precision = tp / float(tp + fp)
-recall = tp / float(tp + fn)
-fscore = (2 * precision * recall) / float(precision + recall)
+#precision = tp / float(tp + fp)
+#recall = tp / float(tp + fn)
+#fscore = (2 * precision * recall) / float(precision + recall)
 
 print(observed, 'matched entries in submission')
 print(total, 'entries in reference file')
 
 print('veracity accuracy:', score)
+print('Micro F1:', f1_score(truths, yhats, average='micro'))
 print('confidence rmse:  ', rmse)
-
-print('precision:', precision)
-print('recall:', recall)
-print('F1:', fscore)
