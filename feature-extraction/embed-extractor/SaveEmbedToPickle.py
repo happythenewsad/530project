3927667fe068e635682baba4a7fee212ce830805
Jason Tang <tjason@seas.upenn.edu>
2018-04-15 21:04:37 -0400

    updated save embed to pickle

--- feature-extraction/embed-extractor/SaveEmbedToPickle.py
+++ feature-extraction/embed-extractor/SaveEmbedToPickle.py
@@ -20,4 +20,4 @@ for key in j:
 	tweet=j[key]['text']
 	tweet2vec[key]=tweet
 
-pickle.dump(tweet2vec,open("feature_embedding_delete.pickle","wb"))
+pickle.dump(tweet2vec,open("feature_embedding.pickle","wb"))
