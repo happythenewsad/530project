b007e9e299f34380bd954fc90e471fb06c90b074
Jason Tang <tjason@seas.upenn.edu>
2018-04-22 17:29:40 -0400

    PCA support

---
+++ feature-extraction/embed-extractor/EmbeddingVector_to_PCA.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import decomposition
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import string
+import pickle 
+
+def chunkIt(seq, num):
+	avg = len(seq) / float(num)
+	out = []
+	last = 0.0
+	zeros = [0]*int(avg)
+
+	while last < len(seq):
+		subVector = seq[int(last):int(last + avg)]
+		if zeros==subVector and last>4700:
+			break
+		out.append(subVector)
+		last += avg
+	return out
+
+
+
+word_embeddings = pd.read_pickle('feature_embedding.pickle')
+
+
+word_embeddings_chunked = {}
+for word in word_embeddings:
+	word_embeddings_chunked[word]=chunkIt(word_embeddings[word],30)
+word_embeddings_pca = {}
+for word in word_embeddings_chunked:
+	pca = decomposition.PCA(n_components=24)
+	x = np.array(word_embeddings_chunked[word])
+	try:
+		x_std = StandardScaler().fit_transform(x)
+		pca.fit_transform(x_std)
+		word_embeddings_pca[word]=pca.singular_values_
+		print(pca.singular_values_)
+	except ValueError:
+		print(word_embeddings_chunked[word])
+		print("UH OH")
+
+pickle.dump(word_embeddings_pca, open("pca_word_embedding.pickle", "wb"))
