7e42d0bf9f8d9e6fb80efa65dcb87359fec52283
Peter Kong <peter@peterkong.com>
2018-04-25 18:04:01 -0400

    fixing tests

--- tests/test_VulgarParser.py
+++ tests/test_VulgarParser.py
@@ -7,7 +7,7 @@ from VulgarExtractor import VulgarExtractor
 class Tester(unittest.TestCase):
     def test_vulgar(self):
         # this path param is relative to test_runner.py
-        vulgarWords = VulgarExtractor.vulgarWords("./feature-extraction/vulgar-extractor/badwords.txt") 
+        vulgarWords = VulgarExtractor.vulgarWords("./code/feature-extraction/vulgar-extractor/badwords.txt") 
 
         nonVulgarLine = "foo bar"
         self.assertEqual(VulgarExtractor.containsVulgar(nonVulgarLine, vulgarWords), False)
